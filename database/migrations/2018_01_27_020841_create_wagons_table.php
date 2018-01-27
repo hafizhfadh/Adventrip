77e3079b9e2213164b97c37d087da12726528bcc
Hafizh Fadhlurrohman <hafizhfadh94@gmail.com>
2018-01-27 09:15:21 +0700

    Add Hafizh Table

---
+++ database/migrations/2018_01_27_020841_create_wagons_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateWagonsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('wagons', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('chair_id');
+            $table->enum('wagon_type', ['Executive', 'Bisnis', 'Economy']);
+            $table->string('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('wagons');
+    }
+}
