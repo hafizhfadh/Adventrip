e663df4171638d738c1f63aa37fd2a71f021105f
Hafizh Fadhlurrohman <hafizhfadh94@gmail.com>
2018-02-03 18:01:23 +0700

    Layout Fix

--- resources/views/_include/aside.blade.php
+++ resources/views/_include/aside.blade.php
@@ -7,30 +7,19 @@
       <li><a>Dashboard</a></li>
       <li><a>Customers</a></li>
     </ul>
-    <p class="menu-label">
-      Administration
-    </p>
+    <p class="menu-label">Trains</p>
     <ul class="menu-list">
-      <li><a>Team Settings</a></li>
-      <li>
-        <a class="is-active">Manage Your Team</a>
-        <ul>
-          <li><a>Members</a></li>
-          <li><a>Plugins</a></li>
-          <li><a>Add a member</a></li>
-        </ul>
-      </li>
-      <li><a>Invitations</a></li>
-      <li><a>Cloud Storage Environment Settings</a></li>
-      <li><a>Authentication</a></li>
+    <li><a>Trains</a></li>
+      <li><a>Schedule Trains</a></li>
+      <li><a>Stations</a></li>
     </ul>
     <p class="menu-label">
       Transactions
     </p>
     <ul class="menu-list">
       <li><a>Payments</a></li>
-      <li><a>Transfers</a></li>
-      <li><a>Balance</a></li>
+      <li><a>Booking</a></li>
+      <li><a>Customer Tickets</a></li>
     </ul>
   </aside>
 </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,19 +5,19 @@
     <div class='carousel carousel-animated carousel-animate-slide' data-autoplay="true">
       <div class='carousel-container'>
         <div class='carousel-item has-background is-active'>
-          <img class="is-background" src="https://wikiki-c4319bfccd.drafts.github.io/images/merry-christmas.jpg" alt="" width="640" height="310" />
+          <img class="is-background" src="https://images.pexels.com/photos/433301/pexels-photo-433301.jpeg?w=1290&h=970&auto=compress&cs=tinysrgb" alt="" width="640" height="310" />
           <div class="title">Merry Christmas</div>
         </div>
         <div class='carousel-item has-background'>
-          <img class="is-background" src="https://wikiki-c4319bfccd.drafts.github.io/images/singer.jpg" alt="" width="640" height="310" />
+          <img class="is-background" src="https://images.pexels.com/photos/78791/railway-bernina-railway-lagalb-bernina-78791.jpeg?w=1290&h=970&auto=compress&cs=tinysrgb" alt="" width="640" height="310" />
           <div class="title">Original Gift: Offer a song with <a href="https://lasongbox.com" target="_blank">La Song Box</a></div>
         </div>
         <div class='carousel-item has-background'>
-          <img class="is-background" src="https://wikiki-c4319bfccd.drafts.github.io/images/sushi.jpg" alt="" width="640" height="310" />
+          <img class="is-background" src="https://images.pexels.com/photos/221281/pexels-photo-221281.jpeg?w=1290&h=970&auto=compress&cs=tinysrgb" alt="" width="640" height="310" />
           <div class="title">Sushi time</div>
         </div>
         <div class='carousel-item has-background'>
-          <img class="is-background" src="https://wikiki-c4319bfccd.drafts.github.io/images/life.jpg" alt="" width="640" height="310" />
+          <img class="is-background" src="https://images.pexels.com/photos/159252/bernina-railway-sweeping-viaduct-brusion-bernina-159252.jpeg?w=1290&h=970&auto=compress&cs=tinysrgb" alt="" width="640" height="310" />
           <div class="title">Life</div>
         </div>
       </div>
@@ -54,6 +54,7 @@
               <div class="control">
                 <input class="input" type="text" placeholder="Text input">
               </div>
+
               <p class="help">This is a help text</p>
             </div>
           </div>
