6c3799a09b9f79ed31a911cd3913e704799a33e3
annisaedrianf <annisaedrianf@gmail.com>
2018-02-07 10:37:47 +0700

    Error Fix

--- resources/views/_include/aside.blade.php
+++ resources/views/_include/aside.blade.php
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 <div class="column m-l-10 m-t-10 is-2 has-shadow">
-=======
-<div class="column m-l-10 m-t-10">
->>>>>>> 28b203589f309d18e367144c9e458021a9317955
   <aside class="menu">
     <p class="menu-label">
       General
